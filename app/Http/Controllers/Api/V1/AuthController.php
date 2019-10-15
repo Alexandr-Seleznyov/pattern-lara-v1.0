1a2799ac2416e9af1a9b0da9521afae5bcefd5dd
Alexandr Seleznyov <seleznyov_ae@ukr.net>
2019-10-15 23:22:23 +0300

    oauth passport

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Auth\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,7 +12,18 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validateDate = $request->validate([
-            'name' => 'required'
+            'name' => 'required|max:55',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create($validateDate);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([
+            'user' => $user,
+            'access_token' => $accessToken,
         ]);
     }
 
